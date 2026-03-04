3d4e6cd76d04bd9123736a78f914d3953de09a5c
lgbaro <legiabao0944324234@gmail.com>
2026-03-04 11:16:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if user_input:
 
     # Gửi cho Groq
     response = client.chat.completions.create(
-        model="llama3-8b-8192",
+       model="llama-3.1-8b-instant",
         messages=[
             {
                 "role": "system",
